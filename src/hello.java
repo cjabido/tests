2a219fb9c12fbbf3e743f7ddb3016a78fced0b3d
Carl <cjabido@gmail.com>
2016-08-07 18:25:39 -0400

    no message

--- src/hello.java
+++ src/hello.java
@@ -1,7 +1,10 @@
-package PACKAGE_NAME;
-
 /**
  * Created by Carl on 8/7/2016.
  */
 public class hello {
+    public static void main(String[] args) {
+        System.out.println("howdy");
+        System.out.println("hi 2");
+
+    }
 }
